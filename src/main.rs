e5b82456817b4b69a9b9126b144e8e88e2dd2c05
Billy Mosis Priambodo <edo.billy@gmail.com>
2024-07-18 13:39:51 +0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,32 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                let mut buffer = [0; 10];
+                let mut buffer = [0; 1024];
                 let res = _stream.read(&mut buffer[..]);
                 match res {
                     Ok(a) => {
                         let x = &buffer[..a];
                         let msg = String::from_utf8(x.to_vec()).expect("error");
                         let msgs: Vec<&str> = msg.split(" ").collect();
-                        print!("{:?}", msg);
                         if msgs[1] == "/" {
                             _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("error");
-                        } else {
-                            _stream
-                                .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
-                                .expect("error");
+                            continue;
                         }
+                        let paths: Vec<&str> = msgs[1]
+                            .split("/")
+                            .collect::<Vec<&str>>()
+                            .into_iter()
+                            .filter(|v| *v != "")
+                            .collect();
+                        if paths[0] == "echo" {
+                            let value = paths[1];
+                            _stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n{}\r\n\r\n", value).as_bytes()).expect("error");
+                            println!("{:?}", paths);
+                            continue;
+                        }
+                        _stream
+                            .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                            .expect("error");
                     }
                     Err(e) => {
                         println!("error: {}", e);
