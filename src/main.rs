d7ab335fbb83f336238727f8826cd3dfc912da47
Billy Mosis Priambodo <edo.billy@gmail.com>
2024-07-18 14:37:21 +0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::{
+    borrow::Borrow,
+    collections::HashMap,
     io::{Read, Write},
     net::TcpListener,
 };
@@ -16,26 +18,50 @@ fn main() {
                     Ok(a) => {
                         let x = &buffer[..a];
                         let msg = String::from_utf8(x.to_vec()).expect("error");
-                        let msgs: Vec<&str> = msg.split(" ").collect();
-                        if msgs[1] == "/" {
+                        let msgs: Vec<&str> = msg.split("\r\n").collect();
+                        let (request_lines, headers) = msgs.split_at(1);
+                        let request_split: Vec<&str> = request_lines[0].split(" ").collect();
+                        let mut header_hash = HashMap::new();
+                        for x in headers {
+                            let split: Vec<&str> = x.split(": ").collect();
+                            if split.len() == 2 {
+                                header_hash.insert(split[0], split[1]);
+                            }
+                        }
+                        let request_path = request_split[1];
+                        if request_path == "/" {
                             _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("error");
                             continue;
                         }
-                        let paths: Vec<&str> = msgs[1]
+                        let paths: Vec<&str> = request_path
                             .split("/")
                             .collect::<Vec<&str>>()
                             .into_iter()
                             .filter(|v| *v != "")
                             .collect();
-                        if paths[0] == "echo" {
-                            let value = paths[1];
-                            _stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", value.len(), value).as_bytes()).expect("error");
-                            println!("{:?}", paths);
-                            continue;
+                        let main_path = paths[0];
+                        match main_path {
+                            "echo" => {
+                                let value = paths[1];
+                                _stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", value.len(), value).as_bytes()).expect("error");
+                                continue;
+                            }
+                            "user-agent" => {
+                                let op = header_hash.get("User-Agent");
+                                match op {
+                                    Some(value) => {
+                                        println!("{}", value);
+                                        _stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", value.len(), value).as_bytes()).expect("error");
+                                    }
+                                    None => continue,
+                                }
+                            }
+                            _ => {
+                                _stream
+                                    .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                                    .expect("error");
+                            }
                         }
-                        _stream
-                            .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
-                            .expect("error");
                     }
                     Err(e) => {
                         println!("error: {}", e);
